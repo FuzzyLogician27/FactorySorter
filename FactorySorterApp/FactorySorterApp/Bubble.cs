d88db7e22e36d18e3415b08cf0982d781dcec82b
JSB-22 <j.s.banyard@gmail.com>
2023-04-17 15:28:12 +0100

    Working on Bubble

---
+++ FactorySorterApp/FactorySorterApp/Bubble.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorySorterApp
+{
+    public class Bubble : ISorter
+    {
+        private ArrayGenerator _arrayGenerator;
+        public Bubble(ArrayGenerator array)
+        {
+            _arrayGenerator = array;
+        }
+        public ArrayGenerator Sort()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
