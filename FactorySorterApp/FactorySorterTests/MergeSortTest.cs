5040a8691ec1d6236b9870847117e7b6db85cc94
Danyal <86931465+danymcdan0@users.noreply.github.com>
2023-04-17 18:09:37 +0100

    Merge sort algo and tests completed

---
+++ FactorySorterApp/FactorySorterTests/MergeSortTest.cs
@@ -0,0 +1,27 @@
+﻿using FactorySorterApp.Models;
+using NUnit.Framework;
+
+namespace FactorySorterTests
+{
+    public class MergeSortTest
+    {
+        private ArrayGenerator? _standardSolvedArray;
+        private ArrayGenerator? _customAgloArray;
+
+        [TestCase(5, 5)]
+        [TestCase(2, 10)]
+        [TestCase(10, 2)]
+        [TestCase(1, 100)]
+        public void GivenArrayGenerator_WhenMergeSort_ReturnsCorrectSortedArray(int a, int b) 
+        {
+            _standardSolvedArray = new ArrayGenerator(a, b);
+            _customAgloArray = new ArrayGenerator(a, b);
+            int[] expectedResult = _standardSolvedArray.SortableArray;
+            Array.Sort(expectedResult);
+            var mergeSort = new Merge(_customAgloArray);
+            mergeSort.Sort();
+            int[] actualResult = _customAgloArray.SortableArray;
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+    }
+}
