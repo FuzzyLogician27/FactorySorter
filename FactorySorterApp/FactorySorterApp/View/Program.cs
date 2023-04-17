f5df8b7e53a22f1180dc92b0c1e20980f13119a9
FuzzyLogician27 <p.ardaghwalter@hotmail.com>
2023-04-17 16:15:55 +0100

    added ToString() override for ArrayGenerator

--- FactorySorterApp/FactorySorterApp/View/Program.cs
+++ FactorySorterApp/FactorySorterApp/View/Program.cs
@@ -1,12 +1,10 @@
-﻿namespace FactorySorterApp.View;
+﻿using FactorySorterApp.Models;
+
+namespace FactorySorterApp.View;
 
 public class Program
 {
-    static void Main()
-    {
-
-
-    }
+    
 
 
 }
